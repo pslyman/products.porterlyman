c8c1087111eb0df53d6dd5e97957e134e52cc3d2
pslyman <dev@porterlyman.com>
2023-10-23 23:03:31 -0600

    nuke old code and unused pages - revamped pizzaapps page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,3 @@
-import { SociallinkerDetailsComponent } from './social-linker/sociallinker-details/sociallinker-details.component';
-import { AppBuilderComponent } from "./services/app-builder/app-builder.component";
-import { SiteBuilderComponent } from "./services/site-builder/site-builder.component";
-import { ServicesComponent } from "./services/services.component";
-import { OTFproductDetailsComponent } from "./openTrackFit/otfproduct-details/otfproduct-details.component";
-import { OpenTrackFit } from "./openTrackFit/openTrackFit.component";
 import { PizzaDoughPremium } from "./pizzaDoughPremium/pizzaDoughPremium.component";
 import { PizzaDoughCalculator } from "./pizzaDoughCalculator/pizzaDoughCalculator.component";
 import { NgModule } from "@angular/core";
@@ -11,12 +5,9 @@ import { RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { PizzaDoughCalculatorChangeLog } from "./pizzaDoughCalculator/pizzadoughcalculatorchangelog/pizzadoughcalculatorchangelog.component";
 import { PremiumchangelogComponent } from "./pizzaDoughPremium/premiumchangelog/premiumchangelog.component";
-import { OpenTrackFitChangelogComponent } from "./openTrackFit/opentrackfitchangelog/opentrackfitchangelog.component";
 import { HomeComponent } from "./home/home.component";
 import { PDPproductDetailsComponent } from "./pizzaDoughPremium/pdpproduct-details/pdpproduct-details.component";
 import { PDCproductDetailsComponent } from "./pizzaDoughCalculator/pdcproduct-details/pdcproduct-details.component";
-import { SiteDemoComponent } from "./services/site-builder/site-demo/site-demo.component";
-import { SocialLinkerComponent } from "./social-linker/social-linker.component";
 import { BothPizzaAppsComponent } from './both-pizza-apps/both-pizza-apps.component';
 
 const routes: Routes = [
@@ -71,80 +62,6 @@ const routes: Routes = [
     path: "PizzaApps",
     component: BothPizzaAppsComponent,
     pathMatch: "full",
-  },
-  {
-    path: "OpenTrackFit",
-    component: OpenTrackFit,
-    children: [
-      {
-        path: "changelog",
-        component: OpenTrackFitChangelogComponent,
-      },
-      { path: "about", component: OTFproductDetailsComponent },
-      {
-        path: "OpenTrackFit/**",
-        redirectTo: "/pageNotFound",
-        pathMatch: "full",
-      },
-      {
-        path: "",
-        redirectTo: "/OpenTrackFit/about",
-        pathMatch: "full",
-      },
-    ],
-  },
-  {
-    path: "SocialLinker",
-    component: SocialLinkerComponent,
-    children: [
-      { path: "about", component: SociallinkerDetailsComponent },
-      {
-        path: "SocialLinker/**",
-        redirectTo: "/pageNotFound",
-        pathMatch: "full",
-      },
-      {
-        path: "",
-        redirectTo: "/SocialLinker/about",
-        pathMatch: "full",
-      },
-    ],
-  },
-  {
-    path: "services",
-    component: ServicesComponent,
-    children: [
-      {
-        path: "site-builder",
-        component: SiteBuilderComponent,
-      },
-      {
-        path: "site-demo",
-        component: SiteDemoComponent,
-        children: [
-          {
-            path: "home",
-            component: SiteBuilderComponent,
-          },
-          /*           {
-                      path: "site-demo",
-                      component: SiteDemoComponent,
-                    }, */
-        ],
-      },
-
-      { path: "app-builder", component: AppBuilderComponent },
-      {
-        path: "services/**",
-        redirectTo: "/pageNotFound",
-        pathMatch: "full",
-      },
-      {
-        path: "",
-        redirectTo: "/home",
-        pathMatch: "full",
-      },
-    ],
   },
   { path: "home", redirectTo: "/home", pathMatch: "full" },
   { path: "", redirectTo: "/home", pathMatch: "full" },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,46 +6,28 @@ import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-import { OpenTrackFit } from "./openTrackFit/openTrackFit.component";
 import { PizzaDoughPremium } from "./pizzaDoughPremium/pizzaDoughPremium.component";
 import { PizzaDoughCalculator } from "./pizzaDoughCalculator/pizzaDoughCalculator.component";
 import { PizzaDoughCalculatorChangeLog } from "./pizzaDoughCalculator/pizzadoughcalculatorchangelog/pizzadoughcalculatorchangelog.component";
 import { PremiumchangelogComponent } from "./pizzaDoughPremium/premiumchangelog/premiumchangelog.component";
-import { OpenTrackFitChangelogComponent } from "./openTrackFit/opentrackfitchangelog/opentrackfitchangelog.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OTFproductDetailsComponent } from './openTrackFit/otfproduct-details/otfproduct-details.component';
 import { PDPproductDetailsComponent } from './pizzaDoughPremium/pdpproduct-details/pdpproduct-details.component';
 import { PDCproductDetailsComponent } from './pizzaDoughCalculator/pdcproduct-details/pdcproduct-details.component';
-import { ServicesComponent } from './services/services.component';
-import { SiteBuilderComponent } from './services/site-builder/site-builder.component';
-import { AppBuilderComponent } from './services/app-builder/app-builder.component';
-import { SiteDemoComponent } from './services/site-builder/site-demo/site-demo.component';
-import { SocialLinkerComponent } from './social-linker/social-linker.component';
-import { SociallinkerDetailsComponent } from './social-linker/sociallinker-details/sociallinker-details.component';
 import { BothPizzaAppsComponent } from './both-pizza-apps/both-pizza-apps.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    OpenTrackFit,
     PizzaDoughPremium,
     PizzaDoughCalculator,
     PizzaDoughCalculatorChangeLog,
     PremiumchangelogComponent,
-    OpenTrackFitChangelogComponent,
     HomeComponent,
-    OTFproductDetailsComponent,
     PDPproductDetailsComponent,
     PDCproductDetailsComponent,
-    ServicesComponent,
-    SiteBuilderComponent,
-    AppBuilderComponent,
-    SiteDemoComponent,
-    SocialLinkerComponent,
-    SociallinkerDetailsComponent,
     BothPizzaAppsComponent
   ],
   imports: [
